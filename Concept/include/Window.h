c1b98d2a69306c1675e0410e53417b7b6db5352a
Tim Jones <tim@freakinabox.com>
2009-11-25 21:22:36 +0000

    Fixed window title-bar clickable area ending at the bottom of the window title text rather than the bottom of the title bar itself
    Signed-off-by: Tim Jones <tim@freakianbox.com>

--- Concept/include/Window.h
+++ Concept/include/Window.h
@@ -14,7 +14,7 @@ class Window : public FBO
 		void SetWindowPos( unsigned int x, unsigned int y ) { m_X = x; m_Y = y; };
 		void MoveWindowPos( unsigned int x_diff, unsigned int y_diff ) { m_X += x_diff; m_Y += y_diff; };
 		bool IsInWindow( unsigned int x, unsigned int y ) { return x >= m_X && x <= m_X + m_Width && y >= m_Y && y <= m_Y + m_Height; };
-		bool IsInActiveArea( unsigned int x, unsigned int y ) { return IsInWindow( x, y ) && y <= m_Y + m_FontTexture->GetHeight(); };
+		bool IsInActiveArea( unsigned int x, unsigned int y ) { return IsInWindow( x, y ) && y <= m_Y + m_FontTexture->GetHeight() + 10; };
 
 	protected:
 		unsigned int m_X, m_Y;
